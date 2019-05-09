06b1c81f1802a06fc0c257bfa8ed3b2b2d430d2d
BenCheng <ben39053372@gmail.com>
2019-05-09 15:20:56 +0800

    first

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div class="header">
+    <h1>棒球運彩分析</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'Header'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col">
+        <h1>主隊</h1>
+      </div>
+      <div class="col">
+        <h1 class='vs'>vs</h1>
+      </div>
+      <div class="col">
+        <h1>客隊</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class='col'>
+        <p>預測結果</p>
+      </div>
+      <div class='col'>
+        <button>預測</button>
+      </div>
+      <div class='col'>
+        <p>預測結果</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+  .col{
+    float:left;
+    width:33%;
+    height: 500px;
+  }
+  button{
+    width:100px;
+    height:50px;
+  }
+  .vs{
+
+  }
+</style>
