11260b6a3c779884eaa07fca748147e03071958f
Chris16862 <30954666+Chris16862@users.noreply.github.com>
2019-05-18 15:51:31 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -9,28 +9,25 @@
 import Main from './components/Main.vue'
 import Header from './components/Header.vue'
 
-
 export default {
   name: 'app',
-  components: { 
+  components: {
     Header,
-    Main,
+    Main
   }
 }
 </script>
 
 <style>
-  #app {
-    font-family: "Avenir", Arial, Helvetica, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-  body{
-    margin:0px;
-    background: url('../static/background.jpg') fixed no-repeat center;
-    background-size: cover;
+#app {
+  text-align: center;
+  color: #2c3e50;
+  width: 100%;
+  float:left;
+  background-image: url('../static/background.jpg');
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
 }
 </style>
